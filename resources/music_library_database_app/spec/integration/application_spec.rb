41c8a9c9b376488cf633193098824e4448747edc
Mergim <mergimberisha0@gmail.com>
2023-05-03 14:16:32 +0100

    cloned repo and passed the test

--- resources/music_library_database_app/spec/integration/application_spec.rb
+++ resources/music_library_database_app/spec/integration/application_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 require "rack/test"
-require_relative '../../app'
+require_relative "../../app"
 
 describe Application do
   # This is so we can use rack-test helper methods.
@@ -10,4 +10,14 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "GET /albums" do
+    it "should return the list of albums" do
+      response = get("/albums")
+
+      expected_response = "Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring"
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
 end
